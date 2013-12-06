1f2fb7df2b561a56f4d783d341995f9e4f8f2619
Jonathan Giddy <jongiddy@gmail.com>
2013-12-06 13:17:51 +0000

    Fixed Windows setup script

--- setup.bat
+++ setup.bat
@@ -3,15 +3,15 @@
 %= Run this batch script from the top-level of the Balcazapy installation. =%
 %= Edit the following lines to the correct values, and add the bin folder  =%
 %= to your PATH. =%
-PYTHON="C:\Python27\python"
+set PYTHON="C:\Python27\python"
 
-BALC="bin\balc"
+set BALC="bin\balc.bat"
 
 del /q %BALC%
 rmdir /q bin
 mkdir bin
 
-echo "@echo off" > %BALC%
-echo "PYTHON=%PYTHON%"
-echo "PYTHONPATH=%CD%\python" >> %BALC%
-echo "%%PYTHON%% %%*" >> %BALC%
+echo @echo off > %BALC%
+echo set PYTHON=%PYTHON% >> %BALC%
+echo set PYTHONPATH=%CD%\python >> %BALC%
+echo %%PYTHON%% %%* >> %BALC%
