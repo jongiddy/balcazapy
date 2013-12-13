f098faf6426d39913980f11adc9a4eeadcfd58f8
Jonathan Giddy <jongiddy@gmail.com>
2013-12-13 14:22:46 +0000

    Perform quoting in a different way

--- setup.bat
+++ setup.bat
@@ -3,9 +3,8 @@
 %= Run this batch script from the top-level of the Balcazapy installation. =%
 %= Edit the following lines to the correct values, and add the bin folder  =%
 %= to your PATH. =%
-set PYTHON="C:\Python27\python"
-rem set BALCAZAPY_HOME="C:\balcazapy"
-set BALCAZAPY_HOME=C:\Users\Jon Giddy\Desktop\balcazapy\balcazapy
+set PYTHON_HOME=C:\Python27
+set BALCAZAPY_HOME=C:\Users\Ann Other\Desktop\balcazapy
 
 set BALC="bin\balc.bat"
 
@@ -13,9 +12,9 @@ del /q %BALC%
 rmdir /q bin
 mkdir bin
 
-echo @echo off > %BALC%
-echo set BALCAZAPY_HOME=%BALCAZAPY_HOME% >> %BALC%
-echo set BALCAZAPROG=%%0 >> %BALC%
-echo set PYTHON=%PYTHON% >> %BALC%
-echo set PYTHONPATH=%CD%\python >> %BALC%
-echo %%PYTHON%% "%BALCAZAPY_HOME%\python\balcaza\T2FlowBuilder.py" %%* >> %BALC%
+echo @echo off> %BALC%
+echo set PYTHON=%PYTHON_HOME%\python>> %BALC%
+echo set BALCAZAPY_HOME=%BALCAZAPY_HOME%>> %BALC%
+echo set BALCAZAPROG=%%0>> %BALC%
+echo set PYTHONPATH=%CD%\python>> %BALC%
+echo "%%PYTHON%%" "%%BALCAZAPY_HOME%%\python\balcaza\T2FlowBuilder.py" %%*>> %BALC%
