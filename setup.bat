096abd30a4ee890c0a1a34be5ce668c4b7ff2bf9
Jonathan Giddy <jongiddy@gmail.com>
2013-12-17 12:40:16 +0000

    Modify Windows setup to use no-space PYTHONPATH

--- setup.bat
+++ setup.bat
@@ -1,10 +1,11 @@
 @echo off
 
 %= Run this batch script from the top-level of the Balcazapy installation. =%
-%= Edit the following lines to the correct values, and add the bin folder  =%
+%= Edit the following line to the correct value, and add the bin folder    =%
 %= to your PATH. =%
 set PYTHON_HOME=C:\Python27
-set BALCAZAPY_HOME=C:\Users\Ann Other\Desktop\balcazapy
+
+for %%f in ("%CD%") do @set BALCAZAPY_HOME=%%~sf
 
 set BALC="bin\balc.bat"
 
@@ -16,5 +17,5 @@ echo @echo off> %BALC%
 echo set PYTHON=%PYTHON_HOME%\python>> %BALC%
 echo set BALCAZAPY_HOME=%BALCAZAPY_HOME%>> %BALC%
 echo set BALCAZAPROG=%%0>> %BALC%
-echo set PYTHONPATH=%CD%\python>> %BALC%
+echo set PYTHONPATH=%%BALCAZAPY_HOME%%\python>> %BALC%
 echo "%%PYTHON%%" -m balcaza.T2FlowBuilder %%*>> %BALC%
