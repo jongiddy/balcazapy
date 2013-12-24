bd9f6e55ce6f585d089ddff11bb8edb6f9d63862
Jonathan Giddy <jongiddy@gmail.com>
2013-12-24 17:23:05 +0000

    Add extra workflows to list library

--- python/balcaza/activity/local/list.py
+++ python/balcaza/activity/local/list.py
@@ -1,6 +1,31 @@
 from balcaza.t2types import *
 from balcaza.t2activity import BeanshellCode
 
+FlattenList = BeanshellCode(
+'''flatten(inputs, outputs, depth) {
+	for (i = inputs.iterator(); i.hasNext();) {
+	    element = i.next();
+		if (element instanceof Collection && depth > 0) {
+			flatten(element, outputs, depth - 1);
+		} else {
+			outputs.add(element);
+		}
+	}
+}
+
+outputlist = new ArrayList();
+
+flatten(inputlist, outputlist, 1);
+''',
+	inputs = dict(
+		inputlist = List[List[String]]
+		),
+	outputs = dict(
+		outputlist = List[String]
+		),
+	name = 'FlattenList'	
+	)
+
 MergeStringListToString = BeanshellCode(
 '''String seperatorString = "\\n";
 if (seperator != void) {
@@ -26,3 +51,21 @@ concatenated = sb.toString();
 	defaultInput = 'stringlist',
 	name = 'MergeStringListToString'
 )
+
+RemoveStringDuplicates = BeanshellCode(
+'''List strippedlist = new ArrayList();
+for (Iterator i = stringlist.iterator(); i.hasNext();) {
+	String item = (String) i.next();
+	if (strippedlist.contains(item) == false) {
+		strippedlist.add(item);
+	}
+}
+''',
+	inputs = dict(
+		stringlist = List[String]
+		),
+	outputs = dict(
+		strippedlist = List[String]
+		),
+	name = 'RemoveStringDuplicates'	
+	)
